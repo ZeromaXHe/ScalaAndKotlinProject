85afe197826831f432850a4d6b4ce1cdf8a0428d
Zhu XH <zhuxiaohe95@163.com>
2022-08-05 19:44:19 +0800

    feature (LeetCodeSolutions): 完成共 7 题 力扣第 628 ~ 630, 632, 633, 636, 637 题

---
+++ LeetCodeSolutions/src/main/scala/com/zerox/from601to800/Solution632.scala
@@ -0,0 +1,71 @@
+package com.zerox.from601to800
+
+/**
+ * @author zhuxi
+ * @since 2022/8/5 16:01
+ * @note
+ * 632. 最小区间 | 难度：困难 | 标签：贪心、数组、哈希表、排序、滑动窗口、堆（优先队列）
+ * 你有 k 个 非递减排列 的整数列表。找到一个 最小 区间，使得 k 个列表中的每个列表至少有一个数包含在其中。
+ *
+ * 我们定义如果 b-a < d-c 或者在 b-a == d-c 时 a < c，则区间 [a,b] 比 [c,d] 小。
+ *
+ * 示例 1：
+ * 输入：nums = [[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
+ * 输出：[20,24]
+ * 解释：
+ * 列表 1：[4, 10, 15, 24, 26]，24 在区间 [20,24] 中。
+ * 列表 2：[0, 9, 12, 20]，20 在区间 [20,24] 中。
+ * 列表 3：[5, 18, 22, 30]，22 在区间 [20,24] 中。
+ *
+ * 示例 2：
+ * 输入：nums = [[1,2,3],[1,2,3],[1,2,3]]
+ * 输出：[1,1]
+ *
+ * 提示：
+ * nums.length == k
+ * 1 <= k <= 3500
+ * 1 <= nums[i].length <= 50
+ * -105 <= nums[i][j] <= 105
+ * nums[i] 按非递减顺序排列
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/smallest-range-covering-elements-from-k-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+object Solution632 {
+  /**
+   * 执行用时：836 ms, 在所有 Scala 提交中击败了 100.00% 的用户
+   * 内存消耗：64.4 MB, 在所有 Scala 提交中击败了 100.00% 的用户
+   * 通过测试用例：88 / 88
+   *
+   * @param nums
+   * @return
+   */
+  def smallestRange(nums: List[List[Int]]): Array[Int] = {
+    // 小顶堆
+    val heap = new scala.collection.mutable.PriorityQueue[(Int, Int)]()(Ordering.by[(Int, Int), Int](_._1).reverse)
+    var max = 0
+    val numIterators = nums.map(_.iterator).toArray
+    for (i <- numIterators.indices) {
+      val first = numIterators(i).next()
+      heap.enqueue((first, i))
+      if (first > max) max = first
+    }
+    val result = Array(heap.head._1, max)
+    while (heap.nonEmpty) {
+      if (!numIterators(heap.head._2).hasNext) {
+        heap.clear()
+      } else {
+        val (_, i) = heap.dequeue()
+        val next = numIterators(i).next()
+        heap.enqueue((next, i))
+        if (next > max) max = next
+        if (max - heap.head._1 < result(1) - result(0)) {
+          result(0) = heap.head._1
+          result(1) = max
+        }
+      }
+    }
+    result
+  }
+}
---
+++ LeetCodeSolutions/src/main/scala/com/zerox/from601to800/Solution637.scala
@@ -0,0 +1,69 @@
+package com.zerox.from601to800
+
+/**
+ * @author zhuxi
+ * @since 2022/8/5 17:48
+ * @note
+ * 637. 二叉树的层平均值 | 难度：简单 | 标签：树、深度优先搜索、广度优先搜索、二叉树
+ * 给定一个非空二叉树的根节点 root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10-5 以内的答案可以被接受。
+ *
+ * 示例 1：
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[3.00000,14.50000,11.00000]
+ * 解释：第 0 层的平均值为 3,第 1 层的平均值为 14.5,第 2 层的平均值为 11 。
+ * 因此返回 [3, 14.5, 11] 。
+ *
+ * 示例 2:
+ * 输入：root = [3,9,20,15,7]
+ * 输出：[3.00000,14.50000,11.00000]
+ *
+ * 提示：
+ * 树中节点数量在 [1, 104] 范围内
+ * -231 <= Node.val <= 231 - 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/average-of-levels-in-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+object Solution637 {
+  /**
+   * Definition for a binary tree node.
+   * | class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+   * |   var value: Int = _value
+   * |   var left: TreeNode = _left
+   * |   var right: TreeNode = _right
+   * | }
+   *
+   * 执行用时：688 ms, 在所有 Scala 提交中击败了 100.00% 的用户
+   * 内存消耗：56.6 MB, 在所有 Scala 提交中击败了 100.00% 的用户
+   * 通过测试用例：66 / 66
+   *
+   * @param root
+   * @return
+   */
+  def averageOfLevels(root: TreeNode): Array[Double] = {
+    val queue = new scala.collection.mutable.Queue[TreeNode]
+    queue += root
+    val result = new scala.collection.mutable.ListBuffer[Double]
+    while (queue.nonEmpty) {
+      var size = queue.size
+      var sum = 0.0
+      val total = size
+      while (size > 0) {
+        val node = queue.dequeue()
+        sum += node.value
+        if (node.left != null) queue.enqueue(node.left)
+        if (node.right != null) queue.enqueue(node.right)
+        size -= 1
+      }
+      result += sum / total
+    }
+    result.toArray
+  }
+
+  class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+    var value: Int = _value
+    var left: TreeNode = _left
+    var right: TreeNode = _right
+  }
+}
